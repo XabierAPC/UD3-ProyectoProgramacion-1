7466fc913062c5bc23542d0d4090c3434a0c6c6c
Ricardo Alvarez Diaz <sproplayerxd@gmail.com>
2023-04-05 09:00:22 +0200

    Fix Wordle

---
+++ ClasesWordle/Palabra.vb
@@ -0,0 +1,20 @@
+﻿Public Class Palabra
+    Implements IEquatable(Of Palabra)
+
+    Public Property Termino As String
+    Public Property Definicion As String()
+
+    Public Overloads Function Equals(other As Palabra) As Boolean Implements IEquatable(Of Palabra).Equals
+        Return other IsNot Nothing AndAlso
+               Me.Termino.Equals(other.Termino, StringComparison.InvariantCultureIgnoreCase)
+    End Function
+
+    Public Sub New(Termino As String)
+        Me.Termino = Termino
+    End Sub
+
+    Public Sub New(Termino As String, Definciones As String())
+        Me.Termino = Termino
+        Me.Definicion = Definciones
+    End Sub
+End Class

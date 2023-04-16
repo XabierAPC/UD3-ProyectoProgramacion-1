8eb41f0ad3dbe9839f43a43cde59ad3f6e91a739
14748 <99936563+14748@users.noreply.github.com>
2023-04-16 22:58:00 +0200

    16/04/2023 - Merge Post Semana Santa
    Juntado todos los commits de todos los integrantes ya que hubo problemas con github en el grupo.

---
+++ WordleClases/Diccionario.vb
@@ -0,0 +1,31 @@
+﻿Imports System.IO
+
+Public Class Diccionario
+    Private ReadOnly Palabras As New List(Of Palabra)
+
+    Public Sub New(rutaArchivoLeer As String)
+        Dim lineas() As String = File.ReadAllLines(rutaArchivoLeer)
+        For Each linea In lineas
+            Dim partes() As String = linea.Split(",")
+            Dim texto As String = partes(0)
+            Dim dificultad As Integer = Integer.Parse(partes(1))
+            Dim numeroLetras As Integer = texto.Length
+            Dim palabra As New Palabra(texto, dificultad, numeroLetras)
+            Palabras.Add(palabra)
+        Next
+    End Sub
+
+    Public Sub AddWord(palabra As Palabra)
+        Me.Palabras.Add(palabra)
+    End Sub
+
+    Public Function GetRandomWord(dificultad As Integer) As Palabra
+        Dim palabrasPosibles = From w In Palabras Where w.Dificultad = dificultad
+        Dim numeroPalabras = palabrasPosibles.Count()
+
+        ''TODO si es 0 return nothing o lanzar excepcion
+
+        Dim randomIndex = New Random().Next(0, numeroPalabras)
+        Return palabrasPosibles(randomIndex)
+    End Function
+End Class
---
+++ WordleClases/Palabra.vb
@@ -0,0 +1,12 @@
+﻿Public Class Palabra
+    Public Property Texto As String
+    Public Property Dificultad As Integer
+    Public Property NumeroLetras As Integer
+
+    Public Sub New(textoPasado As String, dificultadPasada As Integer, numeroLetras As Integer)
+        Me.Texto = textoPasado
+        Me.Dificultad = dificultadPasada
+        Me.NumeroLetras = numeroLetras
+    End Sub
+
+End Class
